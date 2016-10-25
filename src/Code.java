316150c358665cd3ea4628cfc81b44f97a8533a7
Ayushka Partohap <partohap.ayushka@gmail.com>
2016-10-25 15:05:02 +0700

    Added code module and tester

---
+++ src/Code.java
@@ -0,0 +1,7 @@
+package PACKAGE_NAME;
+
+/**
+ * Created by ayushka on 19/10/16.
+ */
+public class Code {
+}
---
+++ src/CodeTester.java
@@ -0,0 +1,7 @@
+package PACKAGE_NAME;
+
+/**
+ * Created by ayushka on 25/10/16.
+ */
+public class CodeTester {
+}
