76366ea130d9e3b5e9040d767506c245d3e05aff
Ayushka Partohap <partohap.ayushka@gmail.com>
2016-10-25 15:13:08 +0700

    Added gitignore

--- src/Code.java
+++ src/Code.java
@@ -1,7 +1,42 @@
-package PACKAGE_NAME;
+import java.util.HashMap;
 
 /**
- * Created by ayushka on 19/10/16.
+ * Code class as specified in Nand2Tetris.org course chapter 6 - Assembler
+ * This class will be part of a main program called "Assembler" also as described
+ * Author : Ayushka Partohap
+ * E-mail : partohap.ayushka@gmail.com
  */
+
 public class Code {
+    private HashMap<String,Integer> destMap;
+    private HashMap<String,Integer> jumpMap;
+    private HashMap<String,Integer> compMap;
+
+    public Code() {
+        generateDestMap();
+        generateJumpMap();
+        generateCompMap();
+    }
+
+    public int dest (String s){
+        return destMap.get(s);
+    }
+
+    private void generateDestMap(){
+        destMap = new HashMap<>();
+        destMap.put(null,  0x000);
+        destMap.put("M",   0x001);
+        destMap.put("D",   0x010);
+        destMap.put("MD",  0x011);
+        destMap.put("A",   0x100);
+        destMap.put("AM",  0x101);
+        destMap.put("AD",  0x110);
+        destMap.put("AMD", 0x111);
+    }
+
+    private void generateJumpMap() {
+    }
+
+    private void generateCompMap() {
+    }
 }
--- src/CodeTester.java
+++ src/CodeTester.java
@@ -1,7 +1,41 @@
-package PACKAGE_NAME;
+import java.io.*;
+import java.net.URL;
 
 /**
- * Created by ayushka on 25/10/16.
+ * Created by ayushka on 10/10/16.
  */
-public class CodeTester {
+public class CodeTester{
+    public static void main(String[] args) {
+        Console console = System.console();
+        if(console==null){
+            System.err.println("No console");
+            System.exit(1);
+        }
+
+
+        String filename = args[0];
+        Parser parser;
+        Code code = new Code();
+        String desthex;
+        try {
+            parser = new Parser(filename);
+            console.printf("%-15s%-10s%-10s%-10s%-10s%-10s\n", "ComType","Symbol","Dest","Comp","Jump","DHX");
+            while (parser.hasMoreCommands()) {
+                parser.advance();
+                desthex = "null";
+                if(parser.dest() != null)
+                    desthex = Integer.toString(code.dest(parser.dest()));
+                console.printf("%-15s%-10s%-10s%-10s%-10s%-10s\n",
+                        parser.commandType(),
+                        parser.symbol(),
+                        parser.dest(),
+                        parser.comp(),
+                        parser.jump(),
+                        desthex);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+    }
 }
